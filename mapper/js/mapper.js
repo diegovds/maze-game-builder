93d4a37879d9d6d0de137b7b952f3057a3c90fd7
Diego Viana <dsantos@inf.ufsm.br>
2022-07-18 03:36:45 -0300

    create help modal

--- mapper/js/mapper.js
+++ mapper/js/mapper.js
@@ -782,3 +782,30 @@ function clickDraw()
     drawGrid();
   }
 }
+
+const switchModal = () => {
+  const modal = document.querySelector('.modal')
+  const actualStyle = modal.style.display
+  if(actualStyle == 'block') {
+    modal.style.display = 'none'
+  }
+  else {
+    modal.style.display = 'block'
+  }
+}
+
+const clickClose = () => {
+  switchModal()
+}
+
+const btn = document.querySelector('.buttonModal')
+if(btn){
+  btn.addEventListener('click', switchModal)
+}
+
+window.onclick = function(event) {
+    const modal = document.querySelector('.modal')
+  if (event.target == modal) {
+    switchModal()
+  }
+}
